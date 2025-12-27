f7f121863d270a2397dd8a8e882ec4be2d8f7e50
ArpaoCeleste <8240558@estg.ipp.pt>
2025-12-27 01:50:18 +0000

    mini melhorias4

--- frontend/src/socket/socket.jsx
+++ frontend/src/socket/socket.jsx
@@ -1,18 +1,29 @@
 import { io } from "socket.io-client";
+import { socketBaseUrl } from "../config/config";
 
-const socket = io();
+const socket = io(socketBaseUrl, {
+    withCredentials: true,
+    transports: ['websocket', 'polling'],
+});
 
 export const initSocket = () => {
-    socket.on("connect", () => {});
-    socket.on("disconnect", () => {});
+    socket.on("connect", () => {
+        console.log("Socket connected:", socket.id);
+    });
+    socket.on("disconnect", () => {
+        console.log("Socket disconnected");
+    });
+    socket.on("connect_error", (err) => {
+        console.log("Socket connection error:", err.message);
+    });
 
     return socket;
 };
 
-export const socketAddListener = (listener = "", callback = () => {}) => {
+export const socketAddListener = (listener = "", callback = () => { }) => {
     socket.on(listener, callback);
 }
 
-export const socketRemoveListener = (listener = "", callback = () => {}) => {
+export const socketRemoveListener = (listener = "", callback = () => { }) => {
     socket.off(listener, callback);
 }
