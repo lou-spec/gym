665ece2489a3a8bf3e06828a275c8eb297f0d39d
ArpaoCeleste <8240558@estg.ipp.pt>
2025-12-27 04:19:42 +0000

    mini melhorias19

--- frontend/src/components/LandingPage/index.jsx
+++ frontend/src/components/LandingPage/index.jsx
@@ -87,7 +87,7 @@ const LandingPage = () => {
                             }}
                             onSwiper={(swiper) => {
                                 setSwiperInstance(swiper);
-                                setTimeout(() => setSwiperReady(true), 5000);
+                                setTimeout(() => setSwiperReady(true), 3000);
                             }}
                             onSlideChange={(swiper) => setActiveIndex(swiper.realIndex % 5)}
                             coverflowEffect={{
